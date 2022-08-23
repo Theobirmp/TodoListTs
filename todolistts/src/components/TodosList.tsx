745130b08d733ef99f13269c7d6639824119e4ca
Theobirmp <theobirmpil@gmail.com>
2022-08-23 12:56:16 +0300

    removed unused stuff

--- todolistts/src/components/TodosList.tsx
+++ todolistts/src/components/TodosList.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import { useTodoContext } from '../hooks/useTodoContext'
-import { Todo } from '../types/todo'
 import { TODOS_ACTIONS } from '../context/TodosContext'
 import SingleTodo from './SingleTodo'
 import {v4 as uuidv4} from 'uuid';
@@ -11,12 +9,7 @@ const TodosList = () => {
   }
   return (
     <>
-    <h1>hello world</h1>
-    <h1>these are the todos</h1>
     <button style={{width:'500px',height:'200px',fontSize:'30px'}} onClick={handleAddTodo}>Add Todo</button>
-    {/* {context?.todos?.map(todo=>(
-        <div>{JSON.stringify(todo)}</div>
-    ))} */}
     {context?.todos?.map(todo=>(
         <SingleTodo {...todo} key={uuidv4()}/>
     ))}
